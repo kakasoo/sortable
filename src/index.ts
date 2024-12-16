4c1a38d0634f714008a4615fc23a93f6bd5e68cc
kakasoo <kscodebase@gmail.com>
2024-12-16 13:30:26 +0900

    fix: if string generic type is union

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { StringPrototype, StringType } from "@kakasoo/proto-typescript";
 import { StringToDeepObject } from "./types";
 
+type Split<U extends string> = U extends infer K extends string
+  ? StringType.Split<K, ".">
+  : never;
+
 export function getSortable<
   Key extends string,
   Direction extends "asc" | "desc"
->(
-  key: Key,
-  direction: Direction
-): StringToDeepObject<StringType.Split<Key, ".">, Direction> {
+>(key: Key, direction: Direction): StringToDeepObject<Split<Key>, Direction> {
   const sortable: any = {};
   const keys = StringPrototype.split(key, ".");
   keys.reduce((acc, key, index, arr) => {
